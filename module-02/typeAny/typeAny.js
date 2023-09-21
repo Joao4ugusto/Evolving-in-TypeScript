52db5c57df6fe63c273f07ca9be5a0af11afbf03
João <joaoaugusto311016@gmail.com>
2023-09-21 14:00:39 -0300

    21-09

---
+++ module-02/typeAny/typeAny.js
@@ -0,0 +1,7 @@
+"use strict";
+// Transpiring TS code to JS: CTRL + SHIFT + B --> TSC BUILD/WATCH
+// Example 01 --> Type Any
+const a = 20;
+const b = 10;
+const result = a + b;
+console.log(result);
---
+++ module-02/typeAny/typeAny.ts
@@ -0,0 +1,15 @@
+// Transpiring TS code to JS: CTRL + SHIFT + B --> TSC BUILD/WATCH
+
+// Example 01 --> Type Any
+const a: any = 20;
+const b: any = 10;
+
+const result1 = a + b;
+console.log(result1);
+
+// Example 02 --> Implicitly inserted any type
+let frase; // Type Any default
+frase = 'Hi, I am from João';
+
+// Example 03 --> 
+
