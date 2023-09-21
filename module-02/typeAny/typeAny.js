a41e5f4004a50ae23d3c95531c6fca67988c2c05
João <joaoaugusto311016@gmail.com>
2023-09-21 16:16:38 -0300

    21-09

--- module-02/typeAny/typeAny.js
+++ module-02/typeAny/typeAny.js
@@ -3,5 +3,15 @@
 // Example 01 --> Type Any
 const a = 20;
 const b = 10;
-const result = a + b;
-console.log(result);
+const result1 = a + b;
+console.log(result1);
+// Example 02 --> Implicitly inserted any type
+let frase; // Type Any default
+frase = 'Hi, I am from João';
+// Example 03 --> When should we use type any
+const forms = {
+    name: 'João',
+    surname: 'Oliveira',
+    age: 20
+};
+console.log(forms);
---
+++ module-02/typeUnknown/typeUnknown.js
@@ -0,0 +1,23 @@
+"use strict";
+// Transpiring TS code to JS: CTRL + SHIFT + B --> TSC BUILD/WATCH
+// Example 01 --> type Unknown
+let valueVariable;
+valueVariable = 'João';
+valueVariable = 123;
+valueVariable = true;
+valueVariable = [];
+// Example 02 --> Error when testing changing the variable type 'Unknown'
+// let value: unknown;
+// let value1: boolean = value;
+// let value1: any[] = value;
+// let value1: number = value;
+// let value1: string = value;
+// Example 03 --> Any vs Unknown
+let somethingAny;
+let somethingUnknown;
+console.log(somethingAny.toFixed());
+// console.log(somethingUnknown.toFixed());
+// Unknown needs to be tested before using the unknown variable
+if (typeof somethingUnknown === 'number') {
+    console.log(somethingUnknown.toFixed());
+}
