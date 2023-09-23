7b966ce47044bec4ddafb26c63657409845d13ea
João <joaoaugusto311016@gmail.com>
2023-09-23 12:23:07 -0300

    23-09

---
+++ module-02/typeNever/typeNever.ts
@@ -0,0 +1,34 @@
+// Transpiring TS code to JS: CTRL + SHIFT + B --> TSC BUILD/WATCH
+
+// Example 01 --> Throw exeption 
+function error(message: string): never {
+  throw new Error(message);
+}
+
+console.log(error('error message 01'));
+
+// Example 02 --> type never automatically inferred
+
+function rejectMessage(){
+  return error('error message 02');
+}
+
+console.log(rejectMessage());
+
+// Example 03 --> Function containing infinite loop returns type 'never'
+
+const loopInfinity = function loop(){
+  while(true){
+    console.log('Hi devs');
+  }
+}
+
+// console.log(loopInfinity());
+
+// Example 04 --> Difference between types: 'never' x 'void'
+
+// const variableVoid: void = null;
+// const variableNever: never = null;
+
+// console.log(variableVoid);
+// console.log(variableNever);
--- module-02/typeNullUndefined/typeNullUndefined.js
+++ module-02/typeNullUndefined/typeNullUndefined.js
@@ -4,10 +4,10 @@
 let variableTestNull = null;
 console.log(variableTestNull);
 console.log(typeof variableTestNull);
-// // Example 02 --> undefined
+// Example 02 --> undefined
 let variableTestUndefined;
 console.log(variableTestUndefined);
 console.log(typeof variableTestUndefined);
-// // Example 03 --> Null vs Undefined
+// Example 03 --> Null vs Undefined
 console.log('Example 01: ', null == undefined);
 console.log('Example 02: ', null === undefined);
