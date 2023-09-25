8383af588e0f58af8bf7c180986576e5a1f255e6
João <joaoaugusto311016@gmail.com>
2023-09-25 17:30:03 -0300

    25-09

---
+++ module-03/loop-while-do-while/loop-while-do-while.ts
@@ -0,0 +1,39 @@
+// Example 01 --> while
+
+let counterWhile = 0;
+
+while(counterWhile < 5){
+  console.log(counterWhile);
+  counterWhile++;
+};
+
+// Example 02 --> while
+
+let counterWhile1 = 1;
+
+while(counterWhile1 <= 20){
+  if(counterWhile1 % 5 == 0){
+    console.log(counterWhile1);
+  }
+  counterWhile1++;
+}
+
+// Example 03 --> while
+
+let counter1 = 0;
+
+const arrayString: string[] = ['joão', 'rose', 'toby'];
+
+while(arrayString[counter1]){
+  console.log('hi ', arrayString[counter1]);
+  counter1++;
+};
+
+// Example 04 --> do while
+
+let counter2 = 0;
+
+do {
+  console.log(counter2);
+  counter2++;
+} while(counter2 < 5);
