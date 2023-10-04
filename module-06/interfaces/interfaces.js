cd57c74e7a617ccd18f780b2bd2041b85356aa51
João <joaoaugusto311016@gmail.com>
2023-10-04 10:11:48 -0300

    04-10

---
+++ module-06/interfaces/interfaces.js
@@ -0,0 +1,42 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function displayName(person) {
+    return `
+ Name: ${person.name},
+ surname: ${person.surname},
+ age: ${person.age}
+ `;
+}
+const joao = {
+    name: 'João',
+    surname: 'Oliveira',
+    age: 20,
+};
+console.log(displayName(joao));
+const book = {
+    title: 'Typescript',
+    author: 'João',
+};
+console.log(book);
+const car = {
+    model: 'fusca',
+    year: 2000
+};
+console.log(car);
+class Cat {
+    name;
+    age;
+    isalive;
+    constructor(name, age, isalive) {
+        this.name = name;
+        this.age = age;
+        this.isalive = isalive;
+    }
+    eat(tipoComida) {
+        console.log(`the cat ${this.name} 
+    is eaten ${tipoComida}`);
+    }
+}
+const cat = new Cat('Totó', 10, true);
+console.log(cat);
+cat.eat('portion');
---
+++ module-06/interfaces/interfaces.ts
@@ -0,0 +1,98 @@
+export{};
+// Example 01 --> Use simple interfaces
+
+interface Person {
+  name: string;
+  surname: string;
+  age: number;
+}
+
+function displayName(person: Person){
+ return `
+ Name: ${person.name},
+ surname: ${person.surname},
+ age: ${person.age}
+ `
+}
+
+const joao = {
+  name: 'João',
+  surname: 'Oliveira',
+  age: 20,
+}
+
+console.log(displayName(joao));
+
+// Example 02 --> optional property usage
+
+interface Book {
+  title: string;
+  author: string;
+  pages?: number;
+}
+
+const book: Book = {
+  title: 'Typescript',
+  author: 'João',
+}
+
+console.log(book);
+
+// Example 03 --> Interface with Read-Only and Optional Properties
+
+interface Car {
+  readonly model: string;
+  year: number;
+  value?: number;
+}
+
+const car: Car = {
+  model: 'fusca',
+  year: 2000
+}
+
+console.log(car);
+
+// Example 04 --> Interface with implements Class
+
+interface Animal {
+  name: string;
+  age: number;
+  isalive: boolean;
+  eat(tipoComida: string): void;
+}
+
+class Cat implements Animal {
+  name: string;
+  age: number;
+  isalive: boolean;
+
+  constructor(name: string, age: number, isalive: boolean) {
+    this.name = name;
+    this.age = age;
+    this.isalive = isalive;
+  }
+
+  eat(tipoComida: string): void {
+    console.log(`the cat ${this.name} 
+    is eaten ${tipoComida}`);
+  }
+}
+
+const cat = new Cat('Totó', 10, true);
+console.log(cat);
+cat.eat('portion');
+
+// ==> Exemplo 05: Interfaces vs Alias Type
+
+interface person_02 {
+  name: string;
+  surname: string;
+  age: number;
+}
+
+type person_03 = {
+  name: string;
+  surname: string;
+  age: number;
+}
