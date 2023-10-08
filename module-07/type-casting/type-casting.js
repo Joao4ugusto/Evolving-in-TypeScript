40a1c5ba4b4604766286f011c4092f0afe92f587
João <joaoaugusto311016@gmail.com>
2023-10-08 15:16:17 -0300

    08-10

---
+++ module-07/type-casting/type-casting.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Example 01 --> type casting: "as"
+const name = 'João Augusto';
+console.log(name.toLowerCase());
+console.log(name.toUpperCase());
+// Example 02 --> type casting: "<>"
+const Car = 'Gol';
+const sizeString = Car.length;
+console.log(sizeString);
---
+++ module-07/type-casting/type-casting.ts
@@ -0,0 +1,15 @@
+export{};
+
+// Example 01 --> type casting: "as"
+
+const name: unknown = 'João Augusto';
+console.log((name as string).toLowerCase());
+console.log((name as string).toUpperCase());
+
+// Example 02 --> type casting: "<>"
+
+const Car = 'Gol';
+
+const sizeString: number = (<string>Car).length;
+console.log(sizeString)
+
